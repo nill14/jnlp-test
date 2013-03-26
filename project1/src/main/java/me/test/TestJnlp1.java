214d3256fd65e6f39fb3770065e1682083b6a4aa
Pavel Sklenak <pavel.sklenak@gmail.com>
2013-03-26 15:12:57 +0100

    use com.github.nill14:webstart-maven-plugin

--- project1/src/main/java/me/test/TestJnlp1.java
+++ project1/src/main/java/me/test/TestJnlp1.java
@@ -1,12 +1,22 @@
 package me.test;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Container;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import javax.swing.AbstractAction;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +24,8 @@ import org.slf4j.LoggerFactory;
 public class TestJnlp1 {
 	
 	private static final Logger log = LoggerFactory.getLogger(TestJnlp1.class);
+	
+	private static final ExecutorService executorService = Executors.newSingleThreadExecutor();
 	
   public static void main(String args[]) {
   	
@@ -24,17 +36,87 @@ public class TestJnlp1 {
   	}
   	
     JFrame frame = new JFrame("Jnlp webstart test1");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    
     JLabel label = new JLabel();
     Container content = frame.getContentPane();
     content.add(label, BorderLayout.CENTER);
     label.setText("Jnln Hello Word");
  
+    Container btnBar = new Container();
+    btnBar.setLayout(new GridBagLayout());
  
-    JButton button = new JButton("Button");
+    JButton btnWork = new JButton("Work 2s");
+    JButton btnClose = new JButton("Close");
  
-    content.add(button, BorderLayout.SOUTH);
+    btnBar.add(btnWork);
+    btnBar.add(btnClose);
+    
+    addListeners(btnWork, btnClose, frame);
+    
+    content.add(btnBar, BorderLayout.SOUTH);
     frame.pack();
     frame.setVisible(true);
   }
+
+	private static void addListeners(final JButton btnWork, final JButton btnClose, final JFrame frame) {
+		
+
+		
+		btnClose.addActionListener(new AbstractAction()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+			}
+		});
+		
+		btnWork.addActionListener(new AbstractAction() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btnWork.setEnabled(false);
+//				btnClose.setEnabled(false);
+				if (e.getSource() instanceof Component) {
+					Component btn = (Component) e.getSource();
+					btn.requestFocus();
+				}
+				
+				
+				executorService.submit(new Runnable() {
+					
+					@Override
+					public void run() {
+						try {
+							Thread.sleep(2000);
+						} catch (InterruptedException e) {
+							throw new RuntimeException(e);
+						}
+						
+						SwingUtilities.invokeLater(new Runnable() {
+					        public void run() {
+					        	btnWork.setEnabled(true);
+								btnClose.setEnabled(true);
+					        }
+						});
+					}
+				});
+				
+			}
+		});
+		
+		KeyStroke enterStroke1 = KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0, false);
+		KeyStroke spaceStroke1 = KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, 0, false);
+		
+		KeyStroke enterStroke2 = KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0, true);
+		KeyStroke spaceStroke2 = KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, 0, true);
+		
+		btnClose.registerKeyboardAction(btnClose.getActionForKeyStroke(spaceStroke1), enterStroke1, JComponent.WHEN_FOCUSED);
+		btnClose.registerKeyboardAction(btnClose.getActionForKeyStroke(spaceStroke2), enterStroke2, JComponent.WHEN_FOCUSED);
+		
+		btnWork.registerKeyboardAction(btnWork.getActionForKeyStroke(spaceStroke1), enterStroke1, JComponent.WHEN_FOCUSED);
+		btnWork.registerKeyboardAction(btnWork.getActionForKeyStroke(spaceStroke2), enterStroke2, JComponent.WHEN_FOCUSED);
+		
+
+	}
 }
